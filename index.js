944b397d7b3c82d5afd457a8815574d701d175de
Jeff Printy <yeffrey@gmail.com>
2018-08-27 20:47:15 -0500

    add sound playing

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const axios = require('axios')
 const bodyParser = require('body-parser')
+const Sound = require('node-aplay')
 const app = express()
 const moment = require('moment')
 
-const dirty = require('dirty');
-const db = dirty('./user.db');
+const dirty = require('dirty')
+const db = dirty('./user.db')
+
+const bitYes = new Sound('./sounds/tron_bit_yes.wav')
+const bitNo = new Sound('./sounds/tron_bit_no.wav')
 
 db.on('load', () => {
   console.log('db loaded');
@@ -29,11 +33,13 @@ app.get('/meds/diditake', (req, res) => {
   const today = moment().format('YYYYMMDD')
   const took = db.get(today);
   if (took) {
+    bitYes.play()
     axios.post('https://maker.ifttt.com/trigger/meds_taken/with/key/dOdmGPvU-ZzrTG5owb8EZz').then(response => {
       console.log('I did take meds today', took)
       res.send(`meds taken ${moment(took).fromNow()}`)
     }).catch(error => console.error('error', error))
   } else {
+    bitNo.play()
     axios.post('https://maker.ifttt.com/trigger/no_meds_taken/with/key/dOdmGPvU-ZzrTG5owb8EZz').then(response => {
       console.log(`no meds taken yet today`)
       res.send(`no meds taken yet today`)
