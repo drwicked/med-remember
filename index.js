0135b34703f3863fc541bf78ff07de6434cc6445
jeff p <yeffrey@gmail.com>
2018-09-05 22:34:34 -0500

    more things

--- index.js
+++ index.js
@@ -10,10 +10,34 @@ const say = require('say')
 
 const db = dirty('./med-remember.db')
 
+var pitft = require("pitft");
+
+var fb = pitft("/dev/fb1"); // Returns a framebuffer in direct mode.  See the clock.js example for double buffering mode
+
+// Clear the screen buffer
+
+var xMax = fb.size().width;
+var yMax = fb.size().height;
+fb.color(1, 1, 1);
+
+// for (var n=0; n<1000; n++) {
+//     var x = Math.random() * (xMax + 32) - 16;
+//     var y = Math.random() * (yMax + 32) - 16;
+
+//     fb.image(x, y, "raspberry-pi-icon.png");
+// }
+
 const bitYes = new Sound('./sounds/yes44.wav')
 const bitNo = new Sound('./sounds/no44.wav')
 
 const token = process.env.POST_TOKEN || 'medrememberposttoken';
+
+const framebufferText = (text) => {
+  fb.clear();
+  fb.font("fantasy", 32, true);
+  fb.text(xMax, yMax/2, text, false, 0, true);
+  setTimeout(() => fb.clear(), 20000)
+}
 
 db.on('load', () => {
   console.log('database loaded')
@@ -54,12 +78,14 @@ app.get('/meds/diditake', (req, res) => {
   const took = db.get(today);
   if (took) {
     bitYes.play();
+    framebufferText(`Took meds: ${moment(took).fromNow()}`)
     say.speak(`meds taken ${moment(took).fromNow()}`);
     console.log('I did take meds today', took)
     res.send(`meds taken ${moment(took).fromNow()}`)
 
   } else {
     bitNo.play()
+    framebufferText(`take your meds`)
     say.speak(`you haven't taken your meds today`)
     res.send(`no meds taken yet today`)
   }
