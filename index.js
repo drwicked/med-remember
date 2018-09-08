159148abbdb0faddf18d71d1ef6f9209bc2e3c1c
Jeff Printy <yeffrey@gmail.com>
2018-09-07 22:36:31 -0500

    fix

--- index.js
+++ index.js
@@ -131,10 +131,11 @@ setInterval(() => {
   } = weatherData;
   fb.font("fantasy", 32, true);
   fb.text(6, 32, weatherString, false, 0, false);
+  fb.font("fantasy", 44, true);
   fb.text(xMax - 6, 32, timeString, false, 0, true);
   fb.font("fantasy", 16, true);
   const sunsetTime = moment(sunset*1000).local().format('h:mm a');
-  fb.text(xMax - 6, 58, sunsetTime, false, 0, true);
+  fb.text(xMax - 6, 64, sunsetTime, false, 0, true);
   fb.text(56, 64, weatherType, false, 0, false);
   fb.image(6, 34, icon);
   fb.text(110, 88, windSpeed, false, 0, false);
