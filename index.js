3bca4d139e13a0862876f5205a85f64b14d79886
Jeff Printy <yeffrey@gmail.com>
2018-09-08 21:33:36 -0500

    fix

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ setInterval(() => {
 
     const ago = shortMoment(medsTook).fromNow();
     fb.font("fantasy", 18);
-    fb.text(32, yMax - 112, ago, true, 45);
+    fb.text(64, yMax - 96, ago, true, 45);
     fb.image(10, yMax - 96, "./images/pill.png");
   }
   fb.font("fantasy", 44, true);
