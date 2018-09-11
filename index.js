c8be4d34bc52480fb467109192d80afda74ff549
Jeff Printy <yeffrey@gmail.com>
2018-09-10 23:31:47 -0500

    update

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ const wget = require('wget-improved')
 const fs = require('fs')
 const $ = require('cheerio')
 const DashButton = require('node-dash-button')
+const tinycolor = require('tinycolor2')
 
 const shortMoment = moment;
 shortMoment.updateLocale('en', {
   relativeTime: {
     future: 'in %s',
     past: '%s',
-    s:  's',
+    s:  '%s',
     ss: '%ss',
     m:  '1m',
     mm: '%dm',
@@ -46,6 +47,8 @@ var fb = pitft("/dev/fb1"); // Returns a framebuffer in direct mode.  See the cl
 var xMax = fb.size().width;
 var yMax = fb.size().height;
 
+const btnObject = {}
+
 // for (var n=0; n<1000; n++) {
 //     var x = Math.random() * (xMax + 32) - 16;
 //     var y = Math.random() * (yMax + 32) - 16;
@@ -63,22 +66,24 @@ const token = process.env.POST_TOKEN || 'medrememberposttoken';
 const weatherDb = dirty('./weather.db')
 const buttonPresses = dirty('./buttons.db')
 const frivolousNatlDays = dirty('./natldays.db')
+const zeroBtns = dirty('./zbutt.db')
 let lastPressed = 0;
-let subscription = button.on('detected', () => {
-  const today = moment().format('YYYYMMDDa')
+let subscription = button.on('detected', async () => {
+  const todaySkewed = moment().subtract(3, 'hours').format('YYYYMMDD')
   ding.play()
-  console.log('button pressed', today);
+  console.log('button pressed', todaySkewed);
   const now = moment().valueOf()
   if ((now - lastPressed) > 60000) {
     lastPressed = now
-    if (buttonPresses.get(today)) {
-      buttonPresses.update(today, (val) => {
+    if (buttonPresses.get(todaySkewed)) {
+      buttonPresses.update(todaySkewed, (val) => {
         return parseInt(val || 0) + 1
       });
     } else {
-      buttonPresses.set(today, 1)
+      buttonPresses.set(todaySkewed, 1)
     }
     buttonPresses.set('lastPressed', lastPressed)
+    await axios.get('http://192.168.1.32:8081/pixel/rainbow').catch(err => console.log(err))
   } else {
     console.log('pressed too recently')
   }
@@ -133,18 +138,19 @@ const toTitleCase = (phrase) => {
 
 const getDays = async (force) => {
   const today = moment().format('YYYYMMDD')
-  if (!frivolousNatlDays.get(today) || force) {
+  const todaysDays = frivolousNatlDays.get(today)
+  if (todaysDays.length > 0) {
+    return todaysDays.map(day => toTitleCase(day))
+  } else {
     return await axios.get('http://nationaldaycalendar.com/latest-posts/').then(({ data }) => {
-      var today = $('.post', data).first();
-      var nationalDays = $('h2.entry-title a', today).text().split(' – ');
+      var todayStr = $('.post', data).first();
+      var nationalDays = $('h2.entry-title a', todayStr).text().split(' – ');
       nationalDays.shift();
       console.log(`:: retrieved ${nationalDays.length} frivolous national holidays and stored in db`)
       const caps = nationalDays.map(day => toTitleCase(day))
       frivolousNatlDays.set(today, caps)
       return caps;
     }).catch((err) => console.log('err', err))
-  } else {
-    return frivolousNatlDays.get(today)
   }
 }
 
@@ -171,7 +177,7 @@ const saveTempData = () => {
 })()
 setInterval(async () => {
   
-  await getDays(true);
+  // await getDays(true);
   saveTempData()
 }, 3600000/4); // quarter hour
 setInterval(async () => {
@@ -199,8 +205,21 @@ setInterval(async () => {
   fb.text(xMax - 6, 35, timeString, false, 0, true);
   fb.font("fantasy", 16, true);
   fb.text(xMax - 6, 96, `${nextHoliday} ${nextHolidayIn}`, false, 0, true);
+
   fb.font("fantasy", 12, true);
-  
+  const maxLength = 32
+  const natlDays = await getDays()
+  if (natlDays) {
+    natlDays.map(async (day, i) => {
+      const dayString = `${day.substring(0, Math.min(maxLength, day.length))}${day.length > maxLength ? '...' : ''}`
+      // console.log('dayString', dayString, xMax - 6, (128 + (i*20)))
+      fb.color(1, 1, 1)
+      fb.text(xMax - 6, (128 + (i*20)), dayString, false, 0, true);
+    })
+  } else {
+    console.log('nope', natlDays);
+  }
+
   fb.font("fantasy", 16, true);
   const sunsetTime = moment(sunset*1000).local().format('h:mm a');
   fb.text(xMax - 6, 64, sunsetTime, false, 0, true);
@@ -214,10 +233,26 @@ setInterval(async () => {
   } else {
     weatherOffsetY = weatherOffsetY - 24
   }
+  const zbtn = zeroBtns.get(moment().format('YYYYMMDD'))
+  if (zbtn && Object.keys(zbtn).length > 0) {
+    const btnOffset = weatherOffsetY + 72
+    Object.keys(zbtn).forEach((key, i) => {
+      const btnPresses = zbtn[key]
+      fb.font("fantasy", 40, true);
+      const {r, g, b} = tinycolor(key).toRgb()
+      fb.color((r/255), (g/255), (b/255));
+      fb.text(22 + (i * 64), btnOffset, btnPresses.length, true, 0, false);
+      fb.font("fantasy", 14 , true);
+      fb.text(14 + (i * 52), btnOffset + 38, shortMoment(btnPresses.slice(-1).pop()).fromNow(), false, 0, false);
+    })
+  }
+  fb.color(1, 1, 1);
+  fb.font("fantasy", 16, true);
   fb.text(110, weatherOffsetY + 24, `${windSpeed}mph`, false, 0, false);
   fb.text(6, weatherOffsetY + 24, `${Math.round(temp_min)}° / ${Math.round(temp_max)}°`, false, 0, false);
   fb.font("fantasy", 12, true);
   fb.text(8, weatherOffsetY + 36, ' lo        hi', false, 0, false);
+  
   fb.font("fantasy", 10, true);
   const uptimeMin = moment.duration(Math.round(process.uptime() / 60), 'minutes').format('h[h]mm[m]');
   fb.text(0, yMax, uptimeMin, false, 0, false);
@@ -231,21 +266,22 @@ setInterval(async () => {
     fb.image(10, yMax - 96, "./images/pill.png");
   }
   fb.font("fantasy", 44, true);
-  fb.text(xMax - 10, yMax - 20, (buttonPresses.get(moment().format('YYYYMMDDa')) || 0), false, 0, true);
+  const todaySkewed = moment().subtract(3, 'hours').format('YYYYMMDD')
+  fb.text(xMax - 10, yMax - 20, (buttonPresses.get(todaySkewed) || 0), false, 0, true);
   fb.font("fantasy", 14, true);
   const last = buttonPresses.get('lastPressed') || moment().valueOf()
   fb.text(xMax, yMax, shortMoment(last).fromNow(), false, 0, true);
   // if (parseInt(moment().format('HH')) >= 20) {
   //   fb.image(100, yMax - 96, "vodka.png");
   // }
-  const tempArray = weatherDb.get(moment().format('YYYYMMDD'))
+  const weatherArray = weatherDb.get(moment().format('YYYYMMDD'))
   const baseX = 200
   const baseY = 220
   const minTemp = 70
   const maxTemp = 105
   const barWidth = 10
   const barPadding = 3
-  tempArray.slice(Math.max(tempArray.length - 16, 1)).forEach((val, i) => {
+  weatherArray.slice(Math.max(weatherArray.length - 16, 1)).forEach((val, i) => {
     const x = baseX + ((barWidth + barPadding) * i)
     const tempPerc = (val - minTemp)/(maxTemp - minTemp)
     const barHeight = Math.floor(tempPerc * maxTemp)
@@ -255,13 +291,6 @@ setInterval(async () => {
     fb.text(x - 2, y - 6, Math.floor(val), false, 0, false);
     fb.color(0, 1, 0)
     fb.rect(x, y, barWidth, barHeight, true);
-  })
-  const maxLength = 32
-  const natlDays = await getDays()  
-  fb.color(1, 1, 1)
-  natlDays.forEach((day, i) => {
-    const dayString = `${day.substring(0, Math.min(maxLength, day.length))}${day.length > maxLength ? '...' : ''}`
-    fb.text(xMax - 6, (128 + (i*20)), dayString, false, 0, true);
   })
 }, 2000);
 
@@ -289,13 +318,36 @@ app.get('/espbutton', (req, res) => {
   res.send('button pressed')
 })
 
-app.post('/meds/took', (req, res) => {
+app.get('/pi/buttons/:pressedButton', (req, res) => {
+  const { pressedButton } = req.params;
+  console.log(`:: ${pressedButton} button pressed`)
+  const today = moment().format('YYYYMMDD')
+  if (zeroBtns.get(today)) {
+    zeroBtns.update(today, (val) => {
+      if (val[pressedButton]) {
+        val[pressedButton].push(moment().valueOf())
+      } else {
+        val[pressedButton] = [moment().valueOf()]
+      }
+      return val
+    })
+  } else {
+    const newObj = {}
+    newObj[pressedButton] = [moment().valueOf()]
+    console.log('init buttons', newObj);
+    zeroBtns.set(today, newObj)
+  }
+  res.status(200).send(`button press submitted ${req.params.pressedButton}`)
+})
+
+app.post('/meds/took', async (req, res) => {
   if (req.body.token === token) {
     // get day-based key
     const today = moment().format('YYYYMMDD')
     // log full timestamp in database
     console.log(':: meds taken', today);
     db.set(today, moment().format());
+    await axios.get('http://192.168.1.32:8081/pixel/rainbow').catch(err => console.log(err))
   } else {
     console.log('invalid token')
     res.status(401);
